efa67c587ee1a58e9d9027a1bde8213ad3760db6
Micro-Chan <64146027+Micro-Chan@users.noreply.github.com>
2022-09-25 16:45:43 +0800

    Update basewl.lua
    wrong fucking script

--- basewl.lua
+++ basewl.lua
@@ -1,102 +1,43 @@
-const http = require('http')
-const hostname = '0.0.0.0'
-const port = 9837;
-const { MongoClient } = require("mongodb");
+_G.key = "324554886267565603717130623217"
+
+local request =  request or syn.request
+local funclib = loadstring(game:HttpGet('https://raw.githubusercontent.com/neycoolman555/cool-screpz/main/bukifuncs.lua'))()
+local haship = funclib:GrabHashedIP()
 
 
-const url = "mongodb+srv://root:.iosucksLOL@whitelist.14ppa.mongodb.net/Whitelist?retryWrites=true&w=majority";
-const client = new MongoClient(url);
+local doc = {
+    key = _G.key,
+    haship = tostring(haship),
+    ostime = os.time()
+}
 
-
-const server = http.createServer((req, res) => {
-    let result = "ai9efj"
-    try{
-        const { headers } = req;
-        let data = '';
-        req.on('data', chunk => {
-            data += chunk;
-        })
-        
-        req.on('end', () => {
-            async function run() {
-                //parse json
-                data = JSON.parse(data) 
-
-                //time
-                const d = new Date();
-                let time = Math.round(d.getTime() / 1000); 
-
-                //uid
-                let sui = headers['syn-fingerprint']
-                if (sui === undefined) {
-                    sui = headers['krnl-fingerprint']
-                }
-                console.log("HWID is "+sui)
-
-                //timechecking
-                let timematch = false
-                if ((time - data.ostime) < 3) {
-                    timematch = true
-                }
-                
-                //getting id
-                let key = data.key
-                key = key.slice(0,-6);
-                key = key.substring(6);
-
-                //size check
-                let size = Object.keys(data).length;
-
-                //connect
-                client.connect();
-                console.log("Connected correctly to server");
-
-                //retrieving entry, checking hwid
-                const database = client.db("whitelist");
-                const collection = database.collection("whitelisted")
-                const query = { duid: key };
-                const options = {
-                  projection: { hwid: 1, lastchange: 1 },
-                };
-                const entry = await collection.findOne(query, options);
-                let hwidallow = true
-                if (entry.hwid != sui) {
-                    if ((time - Number(lastchange)) > 86400) {
-                        collection.updateOne(
-                            { duid: key },
-                            { $set: { hwid: sui, lastchange: time} }
-                        )
-                    } else {  hwidallow = false  }
-                }
-
-                //returning
-                if (timematch && found == 1 && size == 3 && hwidallow) {
-                    collection.updateOne(
-                        { duid: key },
-                        { $push: { IPs: data.haship } }
-                    )
-                    result = "xaea12"
-                } 
-
-                //hwid change time
-                if (!hwidallow) {
-                    result = "hwidgay"
-                }
-
-
-                console.log("---------------------------")
-            }
-        run().catch(console.dir)
-        })  
-    } catch (err) {
-        console.log(err.stack)
-    } 
-    finally {
-        res.end(result)
+local response = request(
+    {
+        Url = "http://130.61.214.19:9837",  -- This website helps debug HTTP requests
+        Method = "POST",
+        Headers = {
+            ["Content-Type"] = "application/json"  -- When sending JSON, set this!
+        },
+        Body = game:GetService("HttpService"):JSONEncode(doc)
     }
-})
+)
 
-server.listen(port,hostname, () => {
-    console.log('Server running')
-    console.log("---------------------------")
-})
+doc = ""
+local check = 0
+for i,v in pairs(response) do
+    if i == "Body" then
+        if v == "hwidgay" then check = 1 end
+        if v == "xaea12" then check = 2 end
+    end
+end
+
+if check == 0 then 
+    while true do end
+elseif check == 1 then
+    game:GetService("Players").LocalPlayer:Kick("HWID can only be changed every 24 hours!")
+elseif check == 2 then
+
+    
+else
+    game:GetService("Players").LocalPlayer:Kick("Unknown error occurred")
+end
